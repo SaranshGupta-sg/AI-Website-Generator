b0adee56c169eae7e57c32da6efc255b0645e977
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-03-22 00:54:44 +0530

    new

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -3,6 +3,7 @@ import type { Project } from "../types";
 import { Loader2Icon, PlusIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { dummyProjects } from "../assets/assets";
+import Footer from "../components/Footer";
 
 const MyProjects = () => {
   const [loading, setLoading] = useState(true);
@@ -125,6 +126,7 @@ const MyProjects = () => {
           </div>
         )}
       </div>
+      <Footer />
     </>
   );
 };
