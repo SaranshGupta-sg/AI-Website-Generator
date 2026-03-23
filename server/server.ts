1f6c2b744c067ea5cbab6e9990477c036a772fc0
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-03-23 19:44:13 +0530

    new

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 
@@ -11,6 +13,7 @@ const corsOption = {
 
 // Middleware
 app.use(cors(corsOption));
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
